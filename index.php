609be766234238871603495247f5322d937a02af
Taylor Ren <taylor.ren@gmail.com>
2015-04-06 10:34:15 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php'; 
+
+$app = new Silex\Application(); 
+$app['debug']=true;
+
+//Twig
+// Register Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path'=>__DIR__.'/views',
+));
+
+$app->get('/', function () use ($app){
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->run();
