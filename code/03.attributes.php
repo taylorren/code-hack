a31dd490d896eaff3b28a53d645c27b380eb221b
Taylor Ren <taylor.ren@gmail.com>
2015-04-09 06:08:42 +0800

    03.03 code.

--- code/03.attributes.php
+++ code/03.attributes.php
@@ -1,4 +1,8 @@
 <?hh
+
+/** 本例程展示了如何获取可反射（Reflectable）项的属性信息。
+* 详细内容见http://taylorr.gitbooks.io/learn-hack-together/content/03.03%20attributes.html。
+*/
 class Foo {
   <<TestFunction, key("6")>>
   public function bar(string $key) {
